1cbd2a2fffb52f16e9a12b7557fe31179703cdff
ivantsovkirill <ivantcovkirill@gmail.com>
2020-01-18 10:41:08 +1000

    1

---
+++ src/ru/ivantsov/chapter01/task01/Main.java
@@ -0,0 +1,13 @@
+package ru.ivantsov.chapter01.task01;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        float PI = 3.1415F;
+        //Ввод данных пользователя с командной строки
+        Scanner scanner = new Scanner(System.in);
+        //Определение переменной и инициализация ввода целочисленной переменной
+        double r = scanner.nextDouble();
+
+    }
+}
