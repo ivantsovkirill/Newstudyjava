a3acad001024f6b7478580fbeca4a4b0c0db16be
ivantsovkirill <ivantcovkirill@gmail.com>
2020-01-18 10:42:35 +1000

    1

--- src/ru/ivantsov/chapter01/task01/Main.java
+++ src/ru/ivantsov/chapter01/task01/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         float PI = 3.1415F;
-        //Ввод данных пользователя с командной строки
+        //Ввод данных пользователя с командной строки рр
         Scanner scanner = new Scanner(System.in);
         //Определение переменной и инициализация ввода целочисленной переменной
         double r = scanner.nextDouble();
